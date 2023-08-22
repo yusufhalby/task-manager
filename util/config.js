da6b832dab2e8316be403595b13ce711bd822ae1
yusufhalby <yosufkhaled@eng.aswu.edu.eg>
2023-08-22 13:44:12 +0300

    config cridintials to .env

--- util/config.js
+++ util/config.js
@@ -1,11 +1,11 @@
 module.exports = {
     firebaseConfig: {
-        apiKey: "AIzaSyDvX22de0AA-1c0o-G2hdRdqx7xXIl1npo",
-        authDomain: "task-manager-53bd1.firebaseapp.com",
-        projectId: "task-manager-53bd1",
-        storageBucket: "task-manager-53bd1.appspot.com",
-        messagingSenderId: "18850677942",
-        appId: "1:18850677942:web:f32f7e7c19d501f4fe1217",
-        measurementId: "G-71EJX65VWN"
+        apiKey: process.env.APIKEY,
+        authDomain: process.env.AUTH_DOMAIN,
+        projectId: process.env.PROJECT_ID,
+        storageBucket: process.env.STORAGE_BUCKET,
+        messagingSenderId: process.env.MESSAGING_SENDER_ID,
+        appId: process.env.APP_ID,
+        measurementId: process.env.MEASUREMENT_ID
     },
 };
